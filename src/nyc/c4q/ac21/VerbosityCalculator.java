7172e6e6c67a117aae87406f9e01baa6d2a0d83d
John Gomez <jgomez818@gmail.com>
2015-03-20 19:27:58 -0400

    Initial commit

--- src/nyc/c4q/ac21/VerbosityCalculator.java
+++ src/nyc/c4q/ac21/VerbosityCalculator.java
@@ -5,23 +5,63 @@ import java.io.FileNotFoundException;
 import java.util.Scanner;
 
 /**
- * Calculate the verbosity of a text
+ * John Gomez 3-20-2015
+ * Calculate and compare the verbosity of two text
+ * files
  */
 public class VerbosityCalculator {
     public static void main(String[] args){
 
         // An abstract representation of a File
-        File file = new File("PATH to text file");
+        File dickens = new File("/Users/c4q-john/Desktop/accesscode/VerbosityCalculator/resources/pg98.txt");
+
+        File melville = new File("/Users/c4q-john/Desktop/accesscode/VerbosityCalculator/resources/pg2701.txt");
+
 
         try{
-            Scanner sc = new Scanner(file);
-            /**
-             *  Your CODE goes here
-             *
-             *
-             *
-             *
-             */
+            Scanner sc = new Scanner(dickens);
+
+            int numWords = 0;
+            int numSentences = 0;
+
+            while (sc.hasNext()) {
+
+                String word = sc.next();
+
+                numWords += 1;
+
+                if (word.endsWith(".")){
+                    numSentences +=1;
+                }
+
+            } System.out.print("Charles Dickens Verbosity Score: "+ numWords/numSentences+"\n");
+            Scanner mv = new Scanner(melville);
+
+            int numWord = 0;
+            int numSentence = 0;
+
+            while (mv.hasNext()) {
+
+                String word = mv.next();
+
+                numWord += 1;
+
+                if (word.endsWith(".")){
+                    numSentence +=1;
+                }
+
+            }System.out.print("Hermal Melville's Verbosity Score: "+ numWord/numSentence+"\n");
+            System.out.print("Seems like Melville's sunken the competition!");
+
+
+
+
+
+
+
+
+
+
 
         }
         catch (FileNotFoundException e){
